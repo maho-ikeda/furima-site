dfbc36619301e45e090ad09312e57d1d9283f830
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-03-07 17:07:55 +0900

    バリデーションの修正、ビューの追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,22 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validates :nickname, presence: true
+
+  validates :email, uniqueness: true
+
+  validates :password, with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヶ一]+\z/, message: '全角カナを使用してください' } do
+    validates :first_name_kana
+    validates :last_name_kana
+  end
+
+  validates :birth_date, presence: true
 end
