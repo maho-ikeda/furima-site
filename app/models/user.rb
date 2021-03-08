23c69217241ac0de259589689f5a6d306eca3e9d
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-03-09 00:46:18 +0900

    エラー表示の作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,12 +1,11 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: true
-  validates :email, uniqueness: true
+  validates :nickname, presence: { message: 'ニックネームを入力してください'}
+  validates :email, uniqueness: { message: 'このメールアドレスは既に登録されています'}
 
-  with_options presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i } do
-    validates :password
-  end
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードは英数字混合で入力してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
     validates :first_name
@@ -17,6 +16,7 @@ class User < ApplicationRecord
     validates :first_name_kana
     validates :last_name_kana
   end
-  
-  validates :birth_date, presence: true
+
+  validates :birth_date, presence: { message: '生年月日を入力してください'}
+
 end
