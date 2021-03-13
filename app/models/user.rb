5253562cbea8a50d510dd53e45b8f2d7293cf995
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-03-13 12:41:37 +0900

    Rubocopでの自動修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,10 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :nickname, presence: { message: 'ニックネームを入力してください'}
-  validates :email, uniqueness: { message: 'このメールアドレスは既に登録されています'}
+  validates :nickname, presence: { message: 'ニックネームを入力してください' }
+  validates :email, uniqueness: { message: 'このメールアドレスは既に登録されています' }
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates_format_of :password, with: PASSWORD_REGEX, message: 'パスワードは英数字混合で入力してください'
 
   with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々]+\z/, message: '全角文字を使用してください' } do
@@ -17,6 +17,5 @@ class User < ApplicationRecord
     validates :last_name_kana
   end
 
-  validates :birth_date, presence: { message: '生年月日を入力してください'}
-
+  validates :birth_date, presence: { message: '生年月日を入力してください' }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,128 +1,128 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe "新規登録/ユーザー情報" do
-    it "ニックネームが必須であること" do
-      user = User.new(nickname: "", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-      last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+  describe '新規登録/ユーザー情報' do
+    it 'ニックネームが必須であること' do
+      user = User.new(nickname: '', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Nickname ニックネームを入力してください")
+      expect(user.errors.full_messages).to include('Nickname ニックネームを入力してください')
     end
 
-    it "メールアドレスが必須であること" do
-      user = User.new(nickname: "abc", email: "", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-      last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'メールアドレスが必須であること' do
+      user = User.new(nickname: 'abc', email: '', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
       expect(user.errors.full_messages).to include("Email can't be blank")
     end
 
-    it "メールアドレスが一意性であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'メールアドレスが一意性であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.save
-      another_user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+      another_user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                              last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       another_user.valid?
-      expect(another_user.errors.full_messages).to include("Email has already been taken", "Email このメールアドレスは既に登録されています")
+      expect(another_user.errors.full_messages).to include('Email has already been taken', 'Email このメールアドレスは既に登録されています')
     end
 
-    it "メールアドレスは、@を含む必要があること" do
-      user = User.new(nickname: "abc", email: "rspectesttest.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'メールアドレスは、@を含む必要があること' do
+      user = User.new(nickname: 'abc', email: 'rspectesttest.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Email is invalid")
+      expect(user.errors.full_messages).to include('Email is invalid')
     end
 
-    it "パスワードが必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "", password_confirmation: "", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'パスワードが必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: '', password_confirmation: '', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Password can't be blank", "Password パスワードは英数字混合で入力してください")
+      expect(user.errors.full_messages).to include("Password can't be blank", 'Password パスワードは英数字混合で入力してください')
     end
 
-    it "パスワードは、6文字以上での入力が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test0", password_confirmation: "test0", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'パスワードは、6文字以上での入力が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test0', password_confirmation: 'test0', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Password is too short (minimum is 6 characters)")
+      expect(user.errors.full_messages).to include('Password is too short (minimum is 6 characters)')
     end
 
-    it "パスワードは、半角英数字混合での入力が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "000000", password_confirmation: "000000", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'パスワードは、半角英数字混合での入力が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: '000000', password_confirmation: '000000', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Password パスワードは英数字混合で入力してください")
+      expect(user.errors.full_messages).to include('Password パスワードは英数字混合で入力してください')
     end
 
-    it "パスワードは、確認用を含めて2回入力すること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'パスワードは、確認用を含めて2回入力すること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: '', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
 
-    it "パスワードとパスワード（確認用）、値の一致が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test01", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'パスワードとパスワード（確認用）、値の一致が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test01', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
       expect(user.errors.full_messages).to include("Password confirmation doesn't match Password")
     end
   end
 
-  describe "新規登録/本人情報確認" do
-    it "ユーザー本名は、名字と名前がそれぞれ必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+  describe '新規登録/本人情報確認' do
+    it 'ユーザー本名は、名字と名前がそれぞれ必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Last name can't be blank", "Last name 全角文字を使用してください")
-      
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+      expect(user.errors.full_messages).to include("Last name can't be blank", 'Last name 全角文字を使用してください')
+
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("First name can't be blank", "First name 全角文字を使用してください")
+      expect(user.errors.full_messages).to include("First name can't be blank", 'First name 全角文字を使用してください')
     end
 
-    it "ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "yamada", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'ユーザー本名は、全角（漢字・ひらがな・カタカナ）での入力が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: 'yamada', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Last name 全角文字を使用してください")
-      
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "rikutaro",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+      expect(user.errors.full_messages).to include('Last name 全角文字を使用してください')
+
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: 'rikutaro',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("First name 全角文字を使用してください")
+      expect(user.errors.full_messages).to include('First name 全角文字を使用してください')
     end
 
-    it "ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'ユーザー本名のフリガナは、名字と名前でそれぞれ必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: '', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Last name kana can't be blank", "Last name kana 全角カナを使用してください")
+      expect(user.errors.full_messages).to include("Last name kana can't be blank", 'Last name kana 全角カナを使用してください')
 
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "", birth_date: "1930-01-01")
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: '', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("First name kana can't be blank", "First name kana 全角カナを使用してください")
+      expect(user.errors.full_messages).to include("First name kana can't be blank", 'First name kana 全角カナを使用してください')
     end
 
-    it "ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "山田", first_name_kana: "リクタロウ", birth_date: "1930-01-01")
+    it 'ユーザー本名のフリガナは、全角（カタカナ）での入力が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: '山田', first_name_kana: 'リクタロウ', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("Last name kana 全角カナを使用してください")
+      expect(user.errors.full_messages).to include('Last name kana 全角カナを使用してください')
 
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "陸太郎", birth_date: "1930-01-01")
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: '陸太郎', birth_date: '1930-01-01')
       user.valid?
-      expect(user.errors.full_messages).to include("First name kana 全角カナを使用してください")
+      expect(user.errors.full_messages).to include('First name kana 全角カナを使用してください')
     end
 
-    it "生年月日が必須であること" do
-      user = User.new(nickname: "abc", email: "rspectest@test.com", password: "test00", password_confirmation: "test00", last_name: "山田", first_name: "陸太郎",
-        last_name_kana: "ヤマダ", first_name_kana: "リクタロウ", birth_date: "")
+    it '生年月日が必須であること' do
+      user = User.new(nickname: 'abc', email: 'rspectest@test.com', password: 'test00', password_confirmation: 'test00', last_name: '山田', first_name: '陸太郎',
+                      last_name_kana: 'ヤマダ', first_name_kana: 'リクタロウ', birth_date: '')
       user.valid?
-      expect(user.errors.full_messages).to include("Birth date 生年月日を入力してください")
+      expect(user.errors.full_messages).to include('Birth date 生年月日を入力してください')
     end
   end
 end
