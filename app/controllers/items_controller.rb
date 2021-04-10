32eca40f20c945181df3faa7fe0274bc38d1dfa3
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-04-10 16:48:43 +0900

    実装の確認
    ログイン/未ログイン状態での画面表示の切り替え
    出品者とログインユーザーが一致しない場合の切り替え
    ¥マークの追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create]
+  before_action :authenticate_user!, only: [:new, :create, :edit]
+  before_action :move_to_index, except: [:index, :show, :create]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -41,6 +42,13 @@ class ItemsController < ApplicationController
   def item_params
     params.require(:item).permit(:image, :name, :price, :explanation, :category_id, :condition_id, :cost_id, :prefecture_id,
                                  :lead_time_id).merge(user_id: current_user.id)
+  end
+
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   # def destroy
