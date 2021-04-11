bbca29b82f2a1573c4d9951625140387ed961e53
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-04-11 10:34:32 +0900

    レビュー修正1回目

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :find_id, only: [:show, :edit, :update, :destroy]
   before_action :move_to_index, except: [:index, :new, :show, :create]
 
