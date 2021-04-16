9f9ab8f02f276cb536b444505a354b174540fc1e
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-04-16 19:58:47 +0900

    ordersコントローラーの設定
    手順2-5.
    購入機能に必要なルーティングを設定する
    手順2-6.
    コントローラーへFormオブジェクトのインスタンスを生成する記述をする

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,14 @@
 class OrdersController < ApplicationController
-  
+  def index
+  end
+
+  def create
+    order = Order.create(order_params)
+    redirect_to "/items/#{order.item.id}" 
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:text).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end
