056353a8a94652eec07489e292487bec27e8e231
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-04-20 00:25:15 +0900

    決済機能の実装
    手順2 決済機能を実装する

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -9,6 +9,7 @@ class OrdersController < ApplicationController
     @item = Item.find(params[:item_id])
     @order_form = OrderForm.new(order_params)
     if @order_form.valid?
+      pay_item
       @order_form.save
       redirect_to root_path
     else
@@ -20,5 +21,14 @@ class OrdersController < ApplicationController
   def order_params
     params.permit(:item_id)
     params.require(:order_form).permit(:postal_code, :prefecture_id, :city, :addresses, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id]).merge(token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = "sk_test_aea641d63c142a247c007cf3"
+    Payjp::Charge.create(
+      amount: @item.price,  # 商品の値段
+      card: order_params[:token],    # カードトークン
+      currency: 'jpy'                 # 通貨の種類（日本円）
+    )
   end
 end
