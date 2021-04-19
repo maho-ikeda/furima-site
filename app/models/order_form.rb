4bdcf3f54f6c533c1275826b9152f8eea2c3e378
bears-magical-book <mahoikeda.dec08vn@gmail.com>
2021-04-20 00:30:28 +0900

    tokenのバリデーションの設定
    手順3 トークンに対するバリデーションを設定する

--- app/models/order_form.rb
+++ app/models/order_form.rb
@@ -2,7 +2,9 @@ class OrderForm
   include ActiveModel::Model
   attr_accessor :item_id, :user_id, :price, :order, :postal_code, :prefecture_id, :city, :addresses, :city, :building, :phone_number, :token
 
+
   with_options presence: true do
+    validates :token
     validates :postal_code, format: { with: /\A\d{3}[-]\d{4}\z/ }
     validates :prefecture_id, numericality: { other_than: 1 }
     validates :city
